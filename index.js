488f87b93dc671a734ca03c0ba10aaf7b6a89cb0
eungyo <eungyo0965@gmail.com>
2024-11-19 15:05:01 +0100

    docs(UI) : set swagger UI in index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swaggerOptions');
+
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // 데이터베이스 연결
 connectDB();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // 미들웨어
 app.use(express.json());
