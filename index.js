a7a171eaa6e52ff13752a3d8a3585a0f0027152a
eungyo <eungyo0965@gmail.com>
2024-11-19 15:18:00 +0100

    docs(index) : update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
 const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./config/swaggerOptions');
+const swaggerSpec = require('./swaggerOptions');
 
 require('dotenv').config();
 
