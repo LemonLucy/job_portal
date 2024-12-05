1fecf98e557373c266098fcc9a61dda8d2b08f5c
eungyo <eungyo0965@gmail.com>
2024-12-05 03:42:54 +0100

    Update port number

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const filterRoutes=require('./routes/filterHistory')
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 
 // 데이터베이스 연결
 connectDB();
