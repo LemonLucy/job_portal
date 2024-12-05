6d8cce0f803f8bf9b04a3f28a69fae39e1bb6383
eungyo <eungyo0965@gmail.com>
2024-12-05 03:55:47 +0100

    Update port number

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/job_portal'); // 옵션 제거
+    await mongoose.connect('mongodb://113.198.66.75:13122/job_portal'); // 옵션 제거
     console.log('MongoDB Connected');
   } catch (err) {
     console.error('Database connection failed:', err);
